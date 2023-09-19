c1e8e2fb05714863df6c05a3ca4f323757747c34
Tracy Truong <tracy18@users.noreply.github.com>
2023-09-19 16:41:53 -0400

    modified the alb module for prod resources

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -24,7 +24,8 @@ locals {
   ]
   
   #ALB
-  alb_subnet_ids      = terraform.workspace == "prod" || terraform.workspace == "stage" ? var.public_subnet_ids : var.private_subnet_ids
+  #alb_subnet_ids      = terraform.workspace == "prod" || terraform.workspace == "stage" ? var.public_subnet_ids : var.private_subnet_ids
+  alb_subnet_ids      = terraform.workspace == "prod" ? var.public_subnet_ids : var.private_subnet_ids
   alb_log_bucket_name = terraform.workspace == "prod" || terraform.workspace == "stage" ? "prod-alb-access-logs" : "nonprod-alb-access-logs"
   cert_types          = "IMPORTED"
   
