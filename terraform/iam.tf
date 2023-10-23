44d5afdcd587d22b5279306b1ed70425a966dd1a
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-10-23 12:06:30 -0400

    updated s3 policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -6,38 +6,19 @@ locals {
 module "iam_assumable_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
   role_permissions_boundary_arn = local.permission_boundary_arn
-  custom_role_trust_policy        = data.aws_iam_policy_document.custom_trust_policy.json
+  trusted_role_services = [
+    "ecs.amazonaws.com"
+  ]
+  truster_role_arns = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/power-user-crdc-hub-${terraform.workspace}-ecs-task-execution-role"
+  ]
   create_role = true
   role_name = "power-user-crdc-hub-${terraform.workspace}-submission-role"
-  create_custom_role_trust_policy = true
   custom_role_policy_arns = [
     module.iam_policy_s3.arn
   ]
 }
 
-data "aws_iam_policy_document" "custom_trust_policy" {
-  statement {
-    effect  = "Allow"
-    actions = [
-      "sts:AssumeRole",
-      "sts:GetSessionToken",
-      "sts:GetAccessKeyInfo"
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/power-user-crdc-hub-${terraform.workspace}-ecs-task-execution-role",
-      ]
-    }
-    principals {
-      type = "Service"
-      identifiers = [
-        "ecs.amazonaws.com"
-      ]
-    }
-  }
-}
 data "aws_iam_policy_document" "s3" {
   statement {
     effect = "Allow"
@@ -51,6 +32,14 @@ data "aws_iam_policy_document" "s3" {
       "${local.submission_bucket_arn}/*",
       local.submission_bucket_arn
     ]
+  }
+  statement {
+    effect  = "Allow"
+    actions = [
+      "sts:GetSessionToken",
+      "sts:GetAccessKeyInfo"
+    ]
+    resources = ["*"]
   }
 }
 module "iam_policy_s3" {
