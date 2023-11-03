a5b07d630b73f71e5b819c819121e1b1134e62a0
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-11-03 11:16:25 -0400

    added list bucket

--- terraform/s3-extra.tf
+++ terraform/s3-extra.tf
@@ -74,7 +74,8 @@ data "aws_iam_policy_document" "task_execution_s3" {
     actions = [
       "s3:GetObject",
       "s3:PutObject",
-      "s3:DeleteObject"
+      "s3:DeleteObject",
+      "s3:ListBucket"
     ]
     resources = [
       aws_s3_bucket.s3_submission.arn,
