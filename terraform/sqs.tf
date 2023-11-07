baaccee145b4969e144c67235412233f1ef85f43
Tracy Truong <tracy18@users.noreply.github.com>
2023-11-07 17:47:12 -0500

    modified sqs module

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "sqs_queues" {
   for_each = var.sqs_queues
-  name    = each.value.name
+  name    = "${local.resource_prefix}-${each.value.queue}-queue.fifo"
   fifo_queue                = true
   delay_seconds             = 0
   max_message_size          = 262144
@@ -17,7 +17,7 @@ EOF
 
 resource "aws_sqs_queue" "dead_letter_queue" {
   for_each = var.sqs_queues
-  name = each.value.dead_letter_queue_name
+  name = "${local.resource_prefix}-${each.value.dead_letter_queue_name}-queue.fifo"
   fifo_queue                = true
 }
 
