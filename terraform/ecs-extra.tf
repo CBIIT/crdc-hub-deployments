92820463a69b7ffbb2d10ca040ed4fd75b6ba3f4
Tracy Truong <tracy18@users.noreply.github.com>
2024-02-16 17:56:05 -0500

    added step scaling policy

--- terraform/ecs-extra.tf
+++ terraform/ecs-extra.tf
@@ -170,7 +170,7 @@ resource "aws_appautoscaling_policy" "sqs_scaling_in_policy" {
     cooldown =  10 # in sec
     metric_aggregation_type = "Maximum"
     step_adjustment {
-      metric_interval_lower_bound = 0
+      metric_interval_upper_bound = 0
       scaling_adjustment         = -1
     }
   }
