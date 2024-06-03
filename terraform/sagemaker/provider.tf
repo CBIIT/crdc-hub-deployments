5d89644d9d2552dbfabb0daca0781b968d65cbe5
Tracy Truong <tracy18@users.noreply.github.com>
2024-06-03 18:19:21 -0400

    added role for sagemaker canvas bedrock

--- terraform/sagemaker/provider.tf
+++ terraform/sagemaker/provider.tf
@@ -2,19 +2,19 @@ terraform {
   backend "s3" {}
 }
 
-provider "aws" {
-  region = var.region
+#provider "aws" {
+#  region = var.region
   
-  default_tags {
-    tags = {
-      EnvironmentTier = terraform.workspace
-      CreatedBy       = "Tracy Truong"
-      ResourceName    = "NCI-crdc-hub-${terraform.workspace}"
-      ManagedBy       = "terraform"
-      Project         = "crdc dh"
-      Backup          = local.level
-      PatchGroup      = local.level
-      ApplicationName = "CRDC Hub"
-    }
-  }
-}
+#  default_tags {
+#    tags = {
+#      EnvironmentTier = terraform.workspace
+#      CreatedBy       = "Tracy Truong"
+#      ResourceName    = "NCI-crdc-hub-${terraform.workspace}"
+#      ManagedBy       = "terraform"
+#      Project         = "crdc dh"
+#      Backup          = local.level
+#      PatchGroup      = local.level
+#      ApplicationName = "CRDC Hub"
+#    }
+#  }
+#}
--- terraform/sagemaker/sagemaker.tf
+++ terraform/sagemaker/sagemaker.tf
@@ -2,8 +2,8 @@ resource "aws_sagemaker_notebook_instance" "sagemaker-notebook" {
   name                  = "tracy-test-notebook"
   instance_type         = "ml.t2.medium"
   role_arn              = aws_iam_role.sagemaker_instance_notebook_role.arn
-  subnet_id             = var.subnet_id
-  security_group_ids    = var.security_group_ids
+#  subnet_id             = var.subnet_id
+#  security_group_ids    = var.security_group_ids
   
   tags = {
     Name = "tracy-test-notebook"
