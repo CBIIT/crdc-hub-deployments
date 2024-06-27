86062aab40718971036254a5b914405fbafb0516
Tracy Truong <tracy18@users.noreply.github.com>
2024-06-27 15:55:16 -0400

    modified the datasync task

--- terraform/data-sync/main.tf
+++ terraform/data-sync/main.tf
@@ -28,10 +28,13 @@ resource "aws_datasync_task" "s3_to_s3" {
   name     = "datasync-task"
   source_location_arn      = aws_datasync_location_s3.source.arn
   destination_location_arn = aws_datasync_location_s3.destination.arn
-  cloudwatch_log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.datasync_log_group.name}:"
+  cloudwatch_log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.datasync_log_group.name}"
 
   options {
-    verify_mode = "POINT_IN_TIME_CONSISTENT"
+    verify_mode = "ONLY_FILES_TRANSFERRED"
+    posix_permissions              = "NONE"
+    uid   = "NONE"
+    gid   = "NONE"
   }
   tags  = var.datasync_tags
 }
