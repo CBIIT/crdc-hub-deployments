6ec99f4aed8a91676ea97a526947743fc70cc93f
Tracy Truong <tracy18@users.noreply.github.com>
2024-07-19 18:58:24 -0400

    added both policies to the datasync iam role

--- terraform/data-sync/iam.tf
+++ terraform/data-sync/iam.tf
@@ -20,12 +20,12 @@ resource "aws_iam_role_policy_attachment" "datasync" {
 }
 
 # create the 2nd role to access s3 in the destination acct
-resource "aws_iam_role" "datasync-s3-access-role" {
+#resource "aws_iam_role" "datasync-s3-access-role" {
 #  assume_role_policy = data.aws_iam_policy_document.assume-role-policy.json
-  assume_role_policy   = var.use_custom_trust_policy ? var.custom_trust_policy: data.aws_iam_policy_document.assume_role_policy.json
-  name = "power-user-${terraform.workspace}-datasync-s3-access-role"
-  permissions_boundary = var.target_account_cloudone ? local.permission_boundary_arn : null
-}
+#  assume_role_policy   = var.use_custom_trust_policy ? var.custom_trust_policy: data.aws_iam_policy_document.assume_role_policy.json
+#  name = "power-user-${terraform.workspace}-datasync-s3-access-role"
+#  permissions_boundary = var.target_account_cloudone ? local.permission_boundary_arn : null
+#}
 
 # create policy for datasync s3
 resource "aws_iam_policy" "datasync-s3-policy" {
@@ -33,10 +33,10 @@ resource "aws_iam_policy" "datasync-s3-policy" {
   policy = data.aws_iam_policy_document.datasync-s3-access.json
 }
 
-#attach policies to the 2nd role to access s3 in the destination acct
+#attach policies to the datasync iam role (not to 2nd role to access s3 in the destination acct)
 resource "aws_iam_role_policy_attachment" "s3-access" {
 #  name       = "power-user-${terraform.workspace}-s3-access-attachment"
-  role = aws_iam_role.datasync-s3-access-role.name
+  role = aws_iam_role.datasync-iam-role.name
   policy_arn = aws_iam_policy.datasync-s3-policy.arn
 }
 
