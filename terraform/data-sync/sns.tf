dcd5fa366d77e380cef69c60bcae692e26dceada
Tracy Truong <tracy18@users.noreply.github.com>
2024-07-23 17:40:40 -0400

    added sns topic

---
+++ terraform/data-sync/sns.tf
@@ -0,0 +1,30 @@
+#create sns topic
+resource "aws_sns_topic" "datasync_status_topic" {
+  name = "datasync_status_topic"
+}
+
+# create subscription
+resource "aws_sns_topic_subscription" "datasync_status_subscription" {
+  topic_arn = aws_sns_topic.datasync_status_topic.arn
+  protocol  = "email"
+  endpoint  = "tracy.truong@nih.gov"
+}
+
+# create evenbridge rule
+resource "aws_cloudwatch_event_rule" "datasync_status_rule" {
+  name        = "datasync_status_rule"
+  description = "Rule to monitor DataSync task execution status changes"
+  event_pattern = jsonencode({
+    "source": ["aws.datasync"],
+    "detail-type": ["DataSync Task Execution State Change"],
+    "detail": {
+      "state": ["SUCCESS", "ERROR"]
+    }
+  })
+}
+
+# add an eventbridge target
+resource "aws_cloudwatch_event_target" "datasync_status_target" {
+  rule      = aws_cloudwatch_event_rule.datasync_status_rule.name
+  arn       = aws_sns_topic.datasync_status_topic.arn
+}
