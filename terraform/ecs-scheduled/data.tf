d28272739579f4bbcf6f390b311002914ac22c43
Tracy Truong <tracy18@users.noreply.github.com>
2024-10-10 23:15:48 -0400

    added version for the target rule

--- terraform/ecs-scheduled/data.tf
+++ terraform/ecs-scheduled/data.tf
@@ -12,7 +12,3 @@ data "aws_iam_role" "schedule_ecs_task_role" {
 data "aws_iam_role" "schedule_ecs_task_execution_role" {
   name = local.execution_task_role_name
 }
-
-data "aws_ecs_task_definition" "latest_task_definition" {
-  task_definition = aws_ecs_task_definition.scheduled_task_definition[each.key].family
-}
